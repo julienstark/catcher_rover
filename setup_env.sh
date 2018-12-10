25f6e25e01beebbff1980a8064eee0dccb3568e4
Julien Thieffry <julien.thieffry@eamslab.com>
2018-12-10 15:53:35 +0900

    setup_env.sh: adding cloud-specific params

--- setup_env.sh
+++ setup_env.sh
@@ -1,3 +1,17 @@
 export CARO_CAPTURE_FOLDER=$(pwd)/client/capture/
 export CARO_INBOX_FOLDER=$(pwd)/server/inbox/
 export CARO_FOLDER=$(pwd)/
+
+export CARO_CLOUD_CONFIG_FILE=clouds.yaml
+
+export CARO_CLOUD_NAME=eams_cloud
+export CARO_CLOUD_INSTANCE_FLAVOR=m1.compute
+export CARO_CLOUD_INSTANCE_BOOT_VOLUME=d316e1b4-7109-4625-88b4-ef5f8ca20533
+export CARO_CLOUD_INSTANCE_BOOT_VOLUME_SIZE=10
+export CARO_CLOUD_INSTANCE_SECURITY_GROUPS=basic_access
+export CARO_CLOUD_INSTANCE_AVAILABILITY_ZONE=nova
+export CARO_CLOUD_INSTANCE_NETWORK=private_network
+export CARO_CLOUD_INSTANCE_IP=192.168.100.163
+
+export CARO_CLOUD_SSH_USERNAME=centos
+export CARO_CLOUD_SSH_KEYFILE=darknet-proto.pem
