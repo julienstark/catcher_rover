0e28b3a9b156db83b66f8cbe394dbd4baceb1f37
Julien Thieffry <julien.thieffry@eamslab.com>
2018-12-13 17:13:55 +0900

    main.py: adding the main server routine.
    Similar to main_client.py, the server routine includes several steps:
        1. Load environ parameters.
        2. Start logger.
        3. Instantiate a detector (load the darknet model).
        4. Start the server socket.
        5. Enter the server listen loop.
            a. Wait for incoming connections.
            b. Receive frame size and send ack.
            c. Receive frame and send ack.
            d. Runs the detector on the image received.
            e. Send bounding boxes to the client.
            f. Close client socket.

---
+++ main.py
@@ -0,0 +1,84 @@
+"""
+Main handler for the catcher_rover server component.
+"""
+
+import os
+
+import utils
+import socks
+import detection
+
+
+def init_environ():
+    """Initialize environment parameters.
+
+    Args:
+        None
+
+    Returns:
+        A dictionary containing all environment variables.
+    """
+
+    caro_loc, inbox_loc = utils.init_environ_folder()
+
+    environ = {'caro_loc':caro_loc,
+               'inbox_loc':inbox_loc,
+               'net':utils.init_environ_net(),
+               'darknet':utils.init_environ_darknet()}
+
+    return environ
+
+
+def start_server():
+    """Runs the catcher_rover main loop."""
+
+    environ = init_environ()
+
+    logger = utils.init_logger('caro_server')
+    logger.info("catcher_rover server - hello")
+
+    logger.info("initializing darknet model")
+    detector = detection.Detection(environ['darknet']['cfg'],
+                                   environ['darknet']['weight'],
+                                   environ['darknet']['data'],
+                                   None)
+
+    logger.info("initializing server socket")
+    server_socket = socks.init_server_socket()
+
+    while True:
+        server_socket.listen(5)
+        logger.info("waiting for incoming connections")
+        client, addr = server_socket.accept()
+        logger.info("incoming connection from %s", str(addr))
+
+        logger.info("receiving frames")
+
+        frame_size = int(socks.receive_bytes_to_string(client))
+        socks.send_msg(client, 'OK FRAME')
+        socks.receive_frame(client, frame_size, environ['inbox_loc'])
+        logger.info("frame received")
+
+        logger.info("starting label detection")
+
+        detector.image = os.path.join(environ['inbox_loc'], "frame.jpg")
+        results = detector.get_coordinates(environ['darknet']['label'])
+
+        logger.info("sending frame received ack")
+        socks.send_msg(client, 'OK FRAME')
+
+        logger.info("frame processing completed")
+
+        logger.info("sending bounding boxes")
+        logger.info("bounding box values: %s", str(results))
+        socks.waiting_for_ack(client, "VECT")
+        socks.send_msg(client, results)
+
+        logger.info("results sent")
+
+        logger.info("closing sockets")
+        client.close()
+
+
+if __name__ == '__main__':
+    start_server()
